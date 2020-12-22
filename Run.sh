98f626bbc0fa7ecb2dced32bbe8a3d5a8b08399a
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2020-12-22 04:53:46 -0800

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#COLOUR
+echo ""
+cyan='\e[0;36m'
+green='\e[0;32m'
+greenl='\e[1;32m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[0;33m'
+yellowl='\e[1;33m'
+BlueF='\e[1;34m' #Biru
+RESET="\033[00m" #normal
+orange='\e[38;5;166m'
+#color-end
+echo -e "$green" ""
+echo -e "$greenl+<><><><><><><><><><><><><><><>>+  ░░░░░░███████]$greenl ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄$green"
+echo -e "<>$green  Proxychains + Tor$green         <>      ▂▄▅█████████▅▄▃▂"
+echo -e "<>$green      By EFX Tv$green             <>     [███████████████████]."
+echo -e "$green""+<><><><><><><><><><><><><><><>>+      ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..."
+echo "" 
+echo -e "$greenl" "──────< o > <><><>─────────────ALL OKAY─────────────<><><>───[ OKAY ]──<><><>──────"
+sleep 1
+echo ""
+echo -e "$greenl""    █▀▀ █─█ █▀▀█ █── █▀▀█ █▀▀█ █▀▀ 　 ▀▀█▀▀ █──█ █▀▀ 　 █───█ █▀▀█ █▀▀█ █── █▀▀▄ "
+echo "    █▀▀ ▄▀▄ █──█ █── █──█ █▄▄▀ █▀▀ 　 ──█── █▀▀█ █▀▀ 　 █▄█▄█ █──█ █▄▄▀ █── █──█ "
+echo "    ▀▀▀ ▀─▀ █▀▀▀ ▀▀▀ ▀▀▀▀ ▀─▀▀ ▀▀▀ 　 ──▀── ▀──▀ ▀▀▀ 　 ─▀─▀─ ▀▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀─"
+echo
+sleep 1
+echo -e "$green▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ "
+echo -e "$green██░▄▄▄░██░▄▄▄██░█████░▄▄▄██░▄▄▀█▄▄░▄▄████░███░██░▄▄▄░██░██░██░▄▄▀████░▄▄▄░██░▄▄░█▄▄░▄▄█▄░▄██░▄▄▄░██░▀██░██░▄▄▄░ "
+echo -e "$green██▄▄▄▀▀██░▄▄▄██░█████░▄▄▄██░██████░██████▄▀▀▀▄██░███░██░██░██░▀▀▄████░███░██░▀▀░███░████░███░███░██░█░█░██▄▄▄▀▀ "
+echo -e "$green██░▀▀▀░██░▀▀▀██░▀▀░██░▀▀▀██░▀▀▄███░████████░████░▀▀▀░██▄▀▀▄██░██░████░▀▀▀░██░██████░███▀░▀██░▀▀▀░██░██▄░██░▀▀▀░ "
+echo -e "$green▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ "
+sleep 1
+echo
+echo ""
+echo "██ █ █ █ █ █ █ █ █ █                     ███████████████████████████████████████████████ "
+echo -e "█$greenl──<>──192.168.0.1:8854<>───────────$greenl[CONNECTED]──────$green █ $greenl SUBSCRIBE  $green██$greenl EFXTV          $green██"
+echo -e "$green""X────────────────────────────────────────────────────────────────────────X"
+echo
+echo -e "$green""────<>────<>─────────[$greenl 1$green ]───────[$greenl CONNECTED$green ]────── [RUN SETUP TOR█████]"
+echo -e "$green"""
+echo -e "$green""────<>────<>─────────[$greenl 2$green ]───────[$greenl CONNECTED$green ]────── [START BROWSING████]"
+echo -e "$green"""
+echo -e "$green""────<>────<>─────────[$greenl 3$green ]───────[$greenl CONNECTED$green ]────── [STOP TOR SERVICE██]"
+echo -e "$green" ""
+echo -e "$green""────<>────<>─────────[$greenl 0$green ]───────[$greenl CONNECTED$green ]────── [EXIT██████████████]"
+#echo -e "$green""────<>────<>─────────[5]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[6]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[7]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[8]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[9]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[x]───────[CONNECTED]────── [What do you need?█]"
+#echo -e "$green""────<>────<>─────────[X]───────[CONNECTED]────── [What do you need?█]"
+echo
+echo -e "$green""X────────────────────────────────────────────────────────────────────────X"
+#echo -e "$green" "" 
+echo -e "█$greenl──<>──192.168.0.1:8854<>───────────$greenl[CONNECTED]──────$green █ $greenl SUBSCRIBE  $green██$greenl EFXTV          $green██"
+
+echo "████████████████████████████████████████████████████████████████████████████████████████"
+echo -e "$greenl" ""
+
+#Menu starts here
+echo -en "────<><>─────────[tor]───────[CONNECTED]────── What do you need? :"
+read man
+case $man in
+    1 ) 
+        echo -e "────<><>─────────[You selected 1]───────[CONNECTED]────── Lets setup Proxychains :"
+sleep 1
+bash stup
+        ;;
+    2 )
+        echo -e "────<><>─────────[You selected 2]───────[CONNECTED]────── Lets start browsing    :"
+sleep 1
+pwd
+bash start
+        ;;
+    3 )
+       echo -e "────<><>─────────[You selected 3]───────[DISCONNECTING]────── Closing the tor service  :"
+sleep 1
+sudo service tor stop 2>&1
+        ;;
+    0 )
+        echo -e "────<><>─────────[You selected 0]───────[Thank you]────── See you again :)       :"
+sleep 1
+exit
+        ;;
+    * )
+        echo -e "$green" "X────────────────────────────────────────────────────────────────────────X"
+        sleep 2
+echo -e "$green" "X────────────────────────<>TRY AGAIN OKAY :)<>───────────────────────────X"
+sleep 2
+echo -e "$green" "X────────────────────────────────────────────────────────────────────────X"
+exit
+esac
+
